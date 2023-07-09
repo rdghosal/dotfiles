5a5626287103123fdc2da5df0a2dca80c8c37728
Rahul Ghosal <rdghosal@gmail.com>
2023-07-08 23:06:31 -0400

    fix: hls for indents and context appeareance

--- nvim/after/plugin/indent-blankline.rc.lua
+++ nvim/after/plugin/indent-blankline.rc.lua
@@ -1,4 +1,8 @@
 require("indent_blankline").setup {
+    char = "",
     show_current_context = true,
     show_current_context_start = false,
 }
+
+-- Disable highlighting of, e.g., tabs during `list` mode.
+vim.api.nvim_set_hl(0, "IndentBlanklineSpaceChar", { fg = "none" })
