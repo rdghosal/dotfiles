fdd1ccbda2f4760b35c5d2bfdd1584e79baf3706
Rahul Ghosal <rdghosal@gmail.com>
2023-07-08 23:15:54 -0400

    fix: highlighting on blanklines within context

--- nvim/after/plugin/indent-blankline.rc.lua
+++ nvim/after/plugin/indent-blankline.rc.lua
@@ -1,7 +1,9 @@
 require("indent_blankline").setup {
     char = "",
+    space_char_blankline = " ",
     show_current_context = true,
     show_current_context_start = false,
+    show_trailing_blankline_indent = false,
 }
 
 -- Disable highlighting of, e.g., tabs during `list` mode.
