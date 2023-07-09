a60d1f85a1e8e4d96602b6265bdb90dbb2d2dfc8
Rahul Ghosal <rdghosal@gmail.com>
2023-07-08 23:22:29 -0400

    feat: add newline character on empty lines

--- nvim/after/plugin/indent-blankline.rc.lua
+++ nvim/after/plugin/indent-blankline.rc.lua
@@ -1,6 +1,6 @@
 require("indent_blankline").setup {
     char = "",
-    space_char_blankline = " ",
+    show_end_of_line = true,
     show_current_context = true,
     show_current_context_start = false,
     show_trailing_blankline_indent = false,
