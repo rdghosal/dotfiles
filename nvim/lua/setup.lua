4ef59ae707de9f866ee9171ec184755273b90367
Rahul Ghosal <rdghosal@gmail.com>
2023-07-12 21:53:22 -0400

    feat: set undofile

--- nvim/lua/setup.lua
+++ nvim/lua/setup.lua
@@ -29,3 +29,6 @@ vim.opt.listchars:append "space:·"
 vim.opt.listchars:append "eol:↵"
 vim.opt.listchars:append "tab:>-"
 vim.opt.guicursor = "n-c:block,v:hor50,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
+
+vim.opt.undodir = os.getenv("HOME") .. "/.nvim/undo"
+vim.opt.undofile = true
