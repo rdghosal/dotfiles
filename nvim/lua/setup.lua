193cac5d23033dd4207190b92a123b67259d25a0
Rahul Ghosal <rdghosal@proton.me>
2026-04-22 07:52:56 -0400

    chore(nvim): disable EOL delims

--- nvim/lua/setup.lua
+++ nvim/lua/setup.lua
@@ -33,7 +33,7 @@ vim.opt.colorcolumn = '80'
 
 vim.opt.list = true
 -- vim.opt.listchars:append "space:·"
-vim.opt.listchars:append('eol:↵')
+-- vim.opt.listchars:append('eol:↵')
 vim.opt.listchars:append('tab:>-')
 vim.opt.cursorline = false
 vim.opt.guicursor =
